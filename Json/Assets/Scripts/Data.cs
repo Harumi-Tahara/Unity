a9b1ab04b82f0150129eda717c76335ac53640fa
Harumi-Tahara <harumi.tahara555@gmail.com>
2017-10-11 23:39:41 +0900

    UI配置、簡易ステータス作成

---
+++ Json/Assets/Scripts/Data.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Data
+{
+
+	[SerializeField]
+	private new string name;
+	public string Name { get { return name; } set { name = value; } }
+
+	public enum HENSIN_TYPE
+	{
+		A,
+		B,
+		C
+	}
+
+	[SerializeField]
+	private HENSIN_TYPE hensinType;
+	public HENSIN_TYPE HensinType { get; set; }
+
+	[SerializeField]
+	private int level;
+	public int Level{ get { return level; } set { level = value < 0 ? 0 : value; }}
+
+	[System.Serializable]
+	public struct StatusData{
+
+		[SerializeField]
+		private int atk;
+		public int Atk{ get { return atk; } set { atk = value < 0 ? 0 : value; }}
+
+		[SerializeField]
+		private int def;
+		public int Def{ get { return def; } set { def = value < 0 ? 0 : value; }}
+	}
+
+	public StatusData Status;
+
+}
+
---
+++ Json/Assets/Scripts/JsonTest.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JsonTest : MonoBehaviour {
+
+	public Data PlayerData;
+	public InputList Input;
+	public Text angou;
+
+	public void UpdatePlayerSatus()
+	{
+		PlayerData.Name = Input.Name.text;
+		PlayerData.HensinType = (Data.HENSIN_TYPE)Input.Type.value;
+		PlayerData.Level = int.Parse(Input.Level.text);
+		PlayerData.Status.Atk = int.Parse(Input.Atk.text);
+		PlayerData.Status.Def = int.Parse(Input.Def.text);
+
+		ToJson();
+		Input.text.text = jsontxt;
+
+		Angou();
+	}
+
+	public string jsontxt;
+
+	public void ToJson(){
+		jsontxt = JsonUtility.ToJson(PlayerData,true);
+
+	}
+
+	public byte[] b;
+	public void Angou(){
+		b = System.Text.Encoding.UTF8.GetBytes(jsontxt);
+		string byteStr = System.BitConverter.ToString(b);
+
+		angou.text = byteStr;
+
+
+	}
+
+}
+
+[System.Serializable]
+public class InputList{
+
+	public InputField Name;
+	public Dropdown Type;
+	public InputField Level;
+	public InputField Atk;
+	public InputField Def;
+
+	public Text text;
+}
+
+[System.Serializable]
+public class OutputList{
+	public Text Name;
+	public Text Type;
+	public Text Level;
+	public Text Atk;
+	public Text Def;
+}
